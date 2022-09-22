014b62b9274e7ff5e6f444dca3d42ebedb80b97c
Matheus Matavelli <matheusmatavellii@gmail.com>
2022-09-22 20:09:19 -0300

    feat: add Typography component

---
+++ src/components/Typography/index.ts
@@ -0,0 +1,22 @@
+import { TextStyle } from 'react-native';
+import styled, { css } from 'styled-components/native';
+
+import { TextVariants, Theme } from '../../theme/lightTheme';
+
+export type TypographyProps = {
+  variant: TextVariants;
+  color?: keyof Theme['palette'];
+  align?: TextStyle['textAlign'];
+};
+
+export const Typography = styled.Text<TypographyProps>`
+  ${({ theme, variant = 'body1', align, color = 'text' }) =>
+    css`
+      font-size: ${theme.textVariants[variant].fontSize}px;
+      font-weight: ${theme.textVariants[variant].fontWeight};
+      line-height: ${theme.textVariants[variant].lineHeight}px;
+      font-family: ${theme.textVariants[variant].fontFamily};
+      color: ${theme.palette[color]};
+      text-align: ${align};
+    `}
+`;
