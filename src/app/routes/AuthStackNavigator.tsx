509d10144f3f490979ee0a5e90e75b4e17fa3105
Matheus Matavelli <matheusmatavellii@gmail.com>
2022-09-22 20:54:03 -0300

    chore: remove react-native-vsg-icon

--- src/app/routes/AuthStackNavigator.tsx
+++ src/app/routes/AuthStackNavigator.tsx
@@ -1,17 +1,29 @@
 import { createStackNavigator } from '@react-navigation/stack';
+import { Header } from '../../components/Header';
 import { Onboarding } from '../../features/auth/screens/Onboarding';
+import { SignIn } from '../../features/auth/screens/SignIn';
 import { AuthStackParamsList } from '../../types/navigation';
 
 const { Navigator, Screen } = createStackNavigator<AuthStackParamsList>();
 
 export function AuthStackNavigator() {
   return (
-    <Navigator
-      screenOptions={{
-        headerShown: false,
-      }}
-    >
-      <Screen name="Onboarding" component={Onboarding} />
+    <Navigator initialRouteName="SignIn">
+      <Screen
+        name="Onboarding"
+        component={Onboarding}
+        options={{
+          headerShown: false,
+        }}
+      />
+      <Screen
+        name="SignIn"
+        component={SignIn}
+        options={{
+          header: Header,
+          headerTitle: 'Sign In',
+        }}
+      />
     </Navigator>
   );
 }
--- src/types/navigation.ts
+++ src/types/navigation.ts
@@ -2,6 +2,7 @@ import { StackNavigationProp } from '@react-navigation/stack';
 
 export type AuthStackParamsList = {
   Onboarding: undefined;
+  SignIn: undefined;
 };
 
 export type AuthScreenProp<T extends keyof AuthStackParamsList> =
