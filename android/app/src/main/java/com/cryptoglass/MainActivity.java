2fc17728b1174d6d790c03e8b23f36adca79159d
Matheus Matavelli <matheusmatavellii@gmail.com>
2022-09-22 14:47:03 -0300

    feat: add onCreate function

--- android/app/src/main/java/com/cryptoglass/MainActivity.java
+++ android/app/src/main/java/com/cryptoglass/MainActivity.java
@@ -1,5 +1,6 @@
 package com.cryptoglass;
 
+import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
@@ -13,6 +14,11 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "CryptoGlass";
+  }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(null);
   }
 
   /**
