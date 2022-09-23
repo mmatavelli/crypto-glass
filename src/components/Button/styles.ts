ecd347ba79a6f37fa31512bec8343900355155d4
Matheus Matavelli <matheusmatavellii@gmail.com>
2022-09-23 11:16:36 -0300

    style: add disabled background-color

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,10 +1,11 @@
 import { RectButton } from 'react-native-gesture-handler';
 import styled from 'styled-components/native';
 
-export const Container = styled(RectButton)`
+export const Container = styled(RectButton)<{ isDisabled?: boolean }>`
   height: 70px;
   align-items: center;
   justify-content: center;
-  background-color: ${({ theme }) => theme.palette.primary};
+  background-color: ${({ theme, isDisabled }) =>
+    isDisabled ? theme.palette.disabled : theme.palette.primary};
   border-radius: ${({ theme }) => theme.shape.borderRadius}px;
 `;
