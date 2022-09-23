ad1f901210509ba4b8b7d81012cf634f66afa75b
Matheus Matavelli <matheusmatavellii@gmail.com>
2022-09-23 11:13:49 -0300

    feat: render ActivityIndicator if isLoading

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,18 +1,26 @@
+import { ActivityIndicator } from 'react-native';
 import { RectButtonProps } from 'react-native-gesture-handler';
-
+import { useTheme } from 'styled-components/native';
 import { Typography } from '../Typography';
 import { Container } from './styles';
 
 export type ButtonProps = RectButtonProps & {
   variant?: 'button1' | 'button2';
+  isLoading?: boolean;
 };
 
-export function Button({ variant, children, ...rest }: ButtonProps) {
+export function Button({ variant, isLoading, children, ...rest }: ButtonProps) {
+  const { palette } = useTheme();
+
   return (
     <Container {...rest}>
-      <Typography variant={variant} color="white">
-        {children}
-      </Typography>
+      {isLoading ? (
+        <ActivityIndicator color={palette.white} />
+      ) : (
+        <Typography variant="button1" color="white">
+          {children}
+        </Typography>
+      )}
     </Container>
   );
 }
