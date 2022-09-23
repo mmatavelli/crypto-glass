6a14f73012779b479eeb541e0819f63efb00909c
Matheus Matavelli <matheusmatavellii@gmail.com>
2022-09-23 10:53:01 -0300

    style: remove font-weight

--- src/components/Typography/index.ts
+++ src/components/Typography/index.ts
@@ -13,7 +13,6 @@ export const Typography = styled.Text<TypographyProps>`
   ${({ theme, variant = 'body1', align, color = 'text' }) =>
     css`
       font-size: ${theme.textVariants[variant].fontSize}px;
-      font-weight: ${theme.textVariants[variant].fontWeight};
       line-height: ${theme.textVariants[variant].lineHeight}px;
       font-family: ${theme.textVariants[variant].fontFamily};
       color: ${theme.palette[color]};
