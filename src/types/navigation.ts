3eef167553decdcf546385bbd7a8687d4da52314
Matheus Matavelli <matheusmatavellii@gmail.com>
2022-09-23 11:44:08 -0300

    chore: add SignUp route

--- src/types/navigation.ts
+++ src/types/navigation.ts
@@ -3,6 +3,7 @@ import { StackNavigationProp } from '@react-navigation/stack';
 export type AuthStackParamsList = {
   Onboarding: undefined;
   SignIn: undefined;
+  SignUp: undefined;
 };
 
 export type AuthScreenProp<T extends keyof AuthStackParamsList> =
