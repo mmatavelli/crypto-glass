0da5e24911bec8f6881386cb8572a5ba33376768
Matheus Matavelli <matheusmatavellii@gmail.com>
2022-09-23 21:03:32 -0300

    feat: add HeaderLeft Component

---
+++ src/app/components/HeaderLeft.tsx
@@ -0,0 +1,43 @@
+import { useNavigation } from '@react-navigation/native';
+import { memo } from 'react';
+import { Pressable } from 'react-native';
+import Feather from 'react-native-vector-icons/Feather';
+import { useTheme } from 'styled-components/native';
+import { Avatar } from '../../components/Avatar';
+
+type HeaderLeftProps = {
+  canGoBack?: boolean;
+};
+
+function HeaderLeftComponent({ canGoBack }: HeaderLeftProps) {
+  const { textVariants, palette } = useTheme();
+  const { goBack, navigate } = useNavigation();
+
+  function handlePress() {
+    if (canGoBack) {
+      goBack();
+    } else {
+      navigate('Menu' as never);
+    }
+  }
+
+  return (
+    <Pressable onPress={handlePress}>
+      {canGoBack ? (
+        <Feather
+          name="arrow-left"
+          size={textVariants.heading2.fontSize}
+          color={palette.text}
+        />
+      ) : (
+        <Avatar
+          source={{
+            uri: 'https://avatars.githubusercontent.com/u/13172299?v=4',
+          }}
+        />
+      )}
+    </Pressable>
+  );
+}
+
+export const HeaderLeft = memo(HeaderLeftComponent);
