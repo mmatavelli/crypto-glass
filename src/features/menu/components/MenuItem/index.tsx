b851f005abc550c7d4331f7c881ebee6ad0fa9eb
Matheus Matavelli <matheusmatavellii@gmail.com>
2022-09-23 22:54:00 -0300

    refactor: remove route prop and add onPress prop

--- src/features/menu/components/MenuItem/index.tsx
+++ src/features/menu/components/MenuItem/index.tsx
@@ -1,23 +1,15 @@
-import { useNavigation } from '@react-navigation/native';
 import { Typography } from '../../../../components/Typography';
-import { ApplicationScreenProp } from '../../../../types/navigation';
 import { Container, Icon } from './styles';
 
 type MenuItemProps = {
   title: string;
   icon: string;
-  route: string;
+  onPress: () => void;
 };
 
-export function MenuItem({ title, icon, route }: MenuItemProps) {
-  const { navigate } = useNavigation<ApplicationScreenProp<'MenuList'>>();
-
-  function handlePress() {
-    navigate(route as any);
-  }
-
+export function MenuItem({ title, icon, onPress }: MenuItemProps) {
   return (
-    <Container onPress={handlePress}>
+    <Container onPress={onPress}>
       <Icon name={icon} />
       <Typography variant="heading3">{title}</Typography>
     </Container>
