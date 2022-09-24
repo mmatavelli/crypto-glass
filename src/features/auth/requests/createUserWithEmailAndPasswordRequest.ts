0d1a647cf41495799df92982a286ebcf1d22df62
Matheus Matavelli <matheusmatavellii@gmail.com>
2022-09-23 21:20:58 -0300

    feat: add createUserWithEmailAndPasswordRequest

---
+++ src/features/auth/requests/createUserWithEmailAndPasswordRequest.ts
@@ -0,0 +1,27 @@
+import auth from '@react-native-firebase/auth';
+import { SignUpFormData } from '../types';
+
+export async function createUserWithEmailAndPasswordRequest({
+  name,
+  email,
+  password,
+}: SignUpFormData) {
+  try {
+    await auth().createUserWithEmailAndPassword(email, password);
+
+    const currentUser = auth().currentUser;
+
+    if (currentUser) {
+      await currentUser.updateProfile({
+        displayName: name,
+      });
+    }
+  } catch (error: any) {
+    if (error.code === 'auth/email-already-in-use') {
+      throw new Error('That email address is already in use!');
+    }
+    if (error.code === 'auth/invalid-email') {
+      throw new Error('That email address is invalid!');
+    }
+  }
+}
