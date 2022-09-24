ab4c47f0b73d7b6c48e1e328c4ad056e251e37e0
Matheus Matavelli <matheusmatavellii@gmail.com>
2022-09-23 21:18:16 -0300

    feat: redirect to ApplicationStackNavigator  if user is authenticated

--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -1,11 +1,30 @@
+import auth, { FirebaseAuthTypes } from '@react-native-firebase/auth';
 import { NavigationContainer } from '@react-navigation/native';
+import { useEffect, useState } from 'react';
 import { navigationTheme } from '../../theme/lightTheme';
+import { ApplicationStackNavigator } from './ApplicationStackNavigator';
 import { AuthStackNavigator } from './AuthStackNavigator';
 
 export function Routes() {
+  const [initializing, setInitializing] = useState(true);
+
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  function onAuthStateChanged(user: FirebaseAuthTypes.User) {
+    setIsAuthenticated(!!user);
+    if (initializing) setInitializing(false);
+  }
+
+  useEffect(() => {
+    const subscriber = auth().onAuthStateChanged(onAuthStateChanged as any);
+    return subscriber;
+  }, []);
+
+  if (initializing) return null;
+
   return (
     <NavigationContainer theme={navigationTheme}>
-      <AuthStackNavigator />
+      {isAuthenticated ? <ApplicationStackNavigator /> : <AuthStackNavigator />}
     </NavigationContainer>
   );
 }
