5d63a4bc7fd8981b4573f09e62a9508d15bc03f7
Matheus Matavelli <matheusmatavellii@gmail.com>
2022-09-24 17:44:31 -0300

    feat: implement QueryClientProvider

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,9 +1,11 @@
 import { StatusBar } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { QueryClientProvider } from 'react-query';
 import { ThemeProvider } from 'styled-components/native';
 
 import { SnackbarProvider } from '../contexts/SnackbarContext';
+import { queryClient } from '../services/reactQuery';
 import { lightTheme } from '../theme/lightTheme';
 import { Routes } from './routes';
 
@@ -14,18 +16,20 @@ export function App() {
         flex: 1,
       }}
     >
-      <SafeAreaProvider>
-        <ThemeProvider theme={lightTheme}>
-          <SnackbarProvider>
-            <StatusBar
-              barStyle="dark-content"
-              translucent
-              backgroundColor="transparent"
-            />
-            <Routes />
-          </SnackbarProvider>
-        </ThemeProvider>
-      </SafeAreaProvider>
+      <QueryClientProvider client={queryClient}>
+        <SafeAreaProvider>
+          <ThemeProvider theme={lightTheme}>
+            <SnackbarProvider>
+              <StatusBar
+                barStyle="dark-content"
+                translucent
+                backgroundColor="transparent"
+              />
+              <Routes />
+            </SnackbarProvider>
+          </ThemeProvider>
+        </SafeAreaProvider>
+      </QueryClientProvider>
     </GestureHandlerRootView>
   );
 }
