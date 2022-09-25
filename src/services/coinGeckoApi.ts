05b508b2ad3a0375050944fa2bf13712696dd990
Matheus Matavelli <matheusmatavellii@gmail.com>
2022-09-25 15:18:37 -0300

    refactor: use coingecko api

--- src/services/coinGeckoApi.ts
+++ src/services/coinGeckoApi.ts
@@ -3,8 +3,4 @@ import Config from 'react-native-config';
 
 export const coinGeckoApi = axios.create({
   baseURL: Config.COINGECKO_API_URL,
-  headers: {
-    'X-RapidAPI-Key': Config.RAPID_API_KEY,
-    'X-RapidAPI-Host': Config.RAPID_API_HOST,
-  },
 });
