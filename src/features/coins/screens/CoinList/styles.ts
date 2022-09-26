a36c3f262265cb8d766e70e6bed2c0b8eff04277
Matheus Matavelli <matheusmatavellii@gmail.com>
2022-09-25 21:13:58 -0300

    feat: add ListEmptyContainer

--- src/features/coins/screens/CoinList/styles.ts
+++ src/features/coins/screens/CoinList/styles.ts
@@ -1,23 +1,11 @@
-import { RectButton } from 'react-native-gesture-handler';
 import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
 `;
 
-export const Tabs = styled.View`
-  flex-direction: row;
-`;
-
-export const TabButton = styled(RectButton)<{
-  isActive?: boolean;
-}>`
-  height: 40px;
-  width: 60px;
-  justify-content: center;
+export const ListEmptyContainer = styled.View`
+  flex: 1;
   align-items: center;
-  margin-right: ${({ theme }) => theme.spacing[2]}px;
-  border-radius: ${({ theme }) => theme.shape.borderRadius}px;
-  background-color: ${({ theme, isActive }) =>
-    isActive ? theme.palette.primary : theme.colors.greekBlue[50]};
+  justify-content: center;
 `;
