69ef3959591de0d9c097355dd51fb69b4175fc88
Matheus Matavelli <matheusmatavellii@gmail.com>
2022-09-25 21:13:05 -0300

    chore: remove unnecessary fields

--- src/features/coins/types/index.ts
+++ src/features/coins/types/index.ts
@@ -1,37 +1,13 @@
-export type Roi = {
-  times: number;
-  currency: string;
-  percentage: number;
-};
-
 export type Coin = {
   id: string;
   symbol: string;
   name: string;
   image: string;
   current_price: number;
-  market_cap: any;
-  market_cap_rank: number;
-  fully_diluted_valuation?: number;
-  total_volume: any;
-  high_24h: number;
-  low_24h: number;
   price_change_24h: number;
   price_change_percentage_24h: number;
   market_cap_change_24h: number;
   market_cap_change_percentage_24h: number;
-  circulating_supply: number;
-  total_supply?: number;
-  max_supply?: number;
-  ath: number;
-  ath_change_percentage: number;
-  ath_date: Date;
-  atl: number;
-  atl_change_percentage: number;
-  atl_date: Date;
-  roi: Roi;
-  last_updated: Date;
-  price_change_percentage_1h_in_currency: number;
 };
 
 export type ListCoinRequest = {
@@ -44,3 +20,25 @@ export type ListCoinRequest = {
 };
 
 export type ListCoinResponse = Coin[];
+
+export type FindCoinByIdResponse = Omit<Coin, 'image'> & {
+  image: {
+    thumb: string;
+    small: string;
+    large: string;
+  };
+  market_data: {
+    current_price: {
+      usd: number;
+    };
+    price_change_percentage_24h: number;
+  };
+};
+
+export type ListOhlcByCoinIdResponse = [
+  number,
+  number,
+  number,
+  number,
+  number,
+][];
