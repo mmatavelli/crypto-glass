5c107536af1ea88bdeafb6347d1dbc8c88a6859a
Matheus Matavelli <matheusmatavellii@gmail.com>
2022-09-25 21:15:07 -0300

    feat: add findCoinChartDataById

---
+++ src/features/coins/hooks/useCoinChartData.ts
@@ -0,0 +1,19 @@
+import { useQuery } from 'react-query';
+
+import { listOhlcByCoinIdRequest } from '../requests/listOhlcByCoinId';
+
+const findCoinChartDataById = async (coinId: string, days: number) => {
+  const result = await listOhlcByCoinIdRequest(coinId, days);
+
+  return result;
+};
+
+export const useCoinChartData = (coinId: string, days = 1) => {
+  return useQuery(
+    ['coinChartData', { coinId, days }],
+    () => findCoinChartDataById(coinId, days),
+    {
+      staleTime: 1000 * 5,
+    },
+  );
+};
