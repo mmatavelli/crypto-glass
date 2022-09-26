ade1d83ff2b27f06a59a6a834c04eca272193aea
Matheus Matavelli <matheusmatavellii@gmail.com>
2022-09-25 21:16:16 -0300

    feat: implement redux Provider

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -2,10 +2,12 @@ import { StatusBar } from 'react-native';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 import { QueryClientProvider } from 'react-query';
+import { Provider } from 'react-redux';
 import { ThemeProvider } from 'styled-components/native';
 
 import { SnackbarProvider } from '../contexts/SnackbarContext';
 import { queryClient } from '../services/reactQuery';
+import { store } from '../store';
 import { lightTheme } from '../theme/lightTheme';
 import { Routes } from './routes';
 
@@ -16,20 +18,22 @@ export function App() {
         flex: 1,
       }}
     >
-      <QueryClientProvider client={queryClient}>
-        <SafeAreaProvider>
+      <Provider store={store}>
+        <QueryClientProvider client={queryClient}>
           <ThemeProvider theme={lightTheme}>
-            <SnackbarProvider>
-              <StatusBar
-                barStyle="dark-content"
-                translucent
-                backgroundColor="transparent"
-              />
-              <Routes />
-            </SnackbarProvider>
+            <SafeAreaProvider>
+              <SnackbarProvider>
+                <StatusBar
+                  barStyle="light-content"
+                  backgroundColor="transparent"
+                  translucent
+                />
+                <Routes />
+              </SnackbarProvider>
+            </SafeAreaProvider>
           </ThemeProvider>
-        </SafeAreaProvider>
-      </QueryClientProvider>
+        </QueryClientProvider>
+      </Provider>
     </GestureHandlerRootView>
   );
 }
